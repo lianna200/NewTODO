6163d931127cebfc99ef2c178c6082c58f6a7558
lianna200 <lianna200@gmail.com>
2017-08-12 00:55:04 -0400

    Update add_task.php

--- add_task.php
+++ add_task.php
@@ -1,28 +1,24 @@
 <?php
 // Get the product data
-$category_id = filter_input(INPUT_POST, 'category_id', FILTER_VALIDATE_INT);
-$code = filter_input(INPUT_POST, 'code');
+$todo_id = filter_input(INPUT_POST, 'todo_id', FILTER_VALIDATE_INT);
 $name = filter_input(INPUT_POST, 'name');
-$price = filter_input(INPUT_POST, 'price', FILTER_VALIDATE_FLOAT);
 
 // Validate inputs
-if ($category_id == null || $category_id == false ||
-        $code == null || $name == null || $price == null || $price == false) {
+if ($todo_id == null || $category_id == false ||
+   $name == null ) {
     $error = "Invalid product data. Check all fields and try again.";
     include('error.php');
 } else {
     require_once('database.php');
 
     // Add the product to the database  
-    $query = 'INSERT INTO products
-                 (categoryID, productCode, productName, listPrice)
+    $query = 'INSERT INTO task
+                 (taskID, taskName)
               VALUES
-                 (:category_id, :code, :name, :price)';
+                 (:todo_id, :name)';
     $statement = $db->prepare($query);
-    $statement->bindValue(':category_id', $category_id);
-    $statement->bindValue(':code', $code);
+    $statement->bindValue(':todo_id', $todo_id);
     $statement->bindValue(':name', $name);
-    $statement->bindValue(':price', $price);
     $statement->execute();
     $statement->closeCursor();
 
