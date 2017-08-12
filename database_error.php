60ca41a2e6f50db3bac7b8ebf3a7b7db5b119776
lianna200 <lianna200@gmail.com>
2017-08-12 01:58:00 -0400

    Update database_error.php

--- database_error.php
+++ database_error.php
@@ -9,14 +9,12 @@
 
 <!-- the body section -->
 <body>
-    <header><h1>My Guitar Shop</h1></header>
+    <header><h1>My TO Do</h1></header>
 
     <main>
         <h1>Database Error</h1>
         <p>There was an error connecting to the database.</p>
-        <p>The database must be installed as described in the appendix.</p>
-        <p>MySQL must be running as described in chapter 1.</p>
-        <p>Error message: <?php echo $error_message; ?></p>
+    
         <p>&nbsp;</p>
     </main>
 
