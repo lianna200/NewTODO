76be5d9adafb0b08d6332c204646a6735ba70729
lianna200 <lianna200@gmail.com>
2017-08-12 00:45:20 -0400

    Update todo_list.php

--- todo_list.php
+++ todo_list.php
@@ -2,8 +2,8 @@
 require_once('database.php');
 
 // Get all categories
-$query = 'SELECT * FROM categories
-                       ORDER BY categoryID';
+$query = 'SELECT * FROM todo
+                       ORDER BY todoID';
 $statement = $db->prepare($query);
 $statement->execute();
 $categories = $statement->fetchAll();
@@ -14,27 +14,27 @@ $statement->closeCursor();
 
 <!-- the head section -->
 <head>
-    <title>My Guitar Shop</title>
+    <title>My TO DO</title>
     <link rel="stylesheet" type="text/css" href="main.css" />
 </head>
 
 <!-- the body section -->
 <body>
-<header><h1>Product Manager</h1></header>
+<header><h1>TAsk Manager</h1></header>
 <main>
-    <h1>Category List</h1>
+    <h1>TO DO List</h1>
     <table>
         <tr>
-            <th>Name</th>
+            <th>TAsk Name</th>
             <th>&nbsp;</th>
         </tr>        
-        <?php foreach ($categories as $category) : ?>
+        <?php foreach ($todo as $todo) : ?>
         <tr>
-            <td><?php echo $category['categoryName']; ?></td>
+            <td><?php echo $todo['todoName']; ?></td>
             <td>
-                <form action="delete_category.php" method="post">
-                    <input type="hidden" name="category_id"
-                           value="<?php echo $category['categoryID']; ?>"/>
+                <form action="delete_todo.php" method="post">
+                    <input type="hidden" name="todo_id"
+                           value="<?php echo $todo['todoID']; ?>"/>
                     <input type="submit" value="Delete"/>
                 </form>
             </td>
@@ -42,20 +42,20 @@ $statement->closeCursor();
         <?php endforeach; ?>    
     </table>
 
-    <h2 class="margin_top_increase">Add Category</h2>
-    <form action="add_category.php" method="post"
-          id="add_category_form">
+    <h2 class="margin_top_increase">Add TO DO</h2>
+    <form action="add_todo.php" method="post"
+          id="add_ctodo_form">
 
-        <label>Name:</label>
+        <label>Task Name:</label>
         <input type="text" name="name" />
-        <input id="add_category_button" type="submit" value="Add"/>
+        <input id="add_todo_button" type="submit" value="Add"/>
     </form>
     
-    <p><a href="index.php">List Products</a></p>
+    <p><a href="index.php">List Task</a></p>
 
 </main>
 <footer>
-    <p>&copy; <?php echo date("Y"); ?> My Guitar Shop, Inc.</p>
+    <p>&copy; <?php echo date("Y"); ?> My TO DO</p>
 </footer>
 </body>
 </html>
